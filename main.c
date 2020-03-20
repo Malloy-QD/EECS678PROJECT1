3d23a1bda85ca49935d437aed84e04d31c6b89b3
QingYang Dong <54829589+Malloy-QD@users.noreply.github.com>
2020-03-20 15:27:53 -0500

    set function and clear whitespace

---
+++ main.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+#include<signal.h>
+#include<sys/types.h>
+#include<sys/mman.h>
+#include<sys/socket.h>
+#include<unistd.h>
+
+static char* env;
+static char* dir;
+static char* cmd;
+static char* path;
+static char* home;
+void removeSpaces(char* str){
+	int count = 0;
+	for (int i =0;str[i];i++)
+		if(str[i]!=' ')
+			str[count++]=str[i];
+
+	str[count]='\0';
+}
+
+void parseCommand(char* command){
+	command = removeSpaces(command);
+	//set command	
+	if(strncmp(command,"set",3)==0){
+	command = command+4;
+		if(strncmp(command,"PATH",4)==0){
+		command = command+4;
+		}
+		printf("%s\n",command);
+		if(strncmp(command,"=",1)==0){
+			command=command+1;
+			char* token = strstr(command,"/");
+			char* path1;
+			char* path2;
+			for(int i = 0;i<sizeof(token);i++){
+			if(token[i]==":"){
+			for(int j=0;j<i-1;j++)
+			path1[i]=token[i];
+			for(int k=i+1;k<sizeof(token);k++)
+			path2[k]=token[k];
+		if(setenv("PATH",path1,0)<0)
+		print();
+		else{
+		path1=getenv("PATH");
+		}
+		
+			}
+					
+			
+			}
+	
+
+		}
+	}
+}
+void setPath(char* newPath){
+	char* token = strtok(newPath,"=");
+	char* type = token;
+	token = strtok(NULL,"\0");
+	char* rest = token;
+
+	if(setenv(type,rest,1)== -1){
+		printf("%s isn't a correct set\n",type);
+	}
+	
+}
